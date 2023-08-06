13a35c4a7dc84491ba0af017774a8ba3bdf0569f
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-08-05 20:47:50 -0600

    adding into index.js

--- index.js
+++ index.js
@@ -16,3 +16,92 @@ const questions = [
         ]
     }
 ]
+
+run()
+function run() {
+    inquirer.prompt(questions).then(answers => {
+        switch(answers.action) {
+            case 'View All Enemies':
+                viewAllEnemies();
+                break;
+            case 'View MyList':
+                viewMyList();
+                break;
+            case 'Add to MyList':
+                addToMyList();
+                break;
+            case 'Remove from MyList':
+                removeFromMyList();
+                break;
+            case 'Exit':
+                connection.end();
+                break;
+        }
+    })
+}
+
+const viewAllEnemies = () => {
+    const sql = `SELECT * FROM enemies`;
+    db.query(sql, (err, response) => {
+        if(err) throw err;
+        console.table(response);
+        run();
+    }
+    )
+    console.table(result);
+    run();
+}
+
+const viewMyList = () => {
+    const sql = `SELECT * FROM mylist`;
+    db.query(sql, (err, response) => {
+        if(err) throw err;
+        console.table(response);
+        run();
+    }
+    )
+}
+
+const addToMyList = () => {
+    const sql = `SELECT * FROM enemies`;
+    db.query(sql, (err, response) => {
+        if(err) throw err;
+        console.table(response);
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the ID of the enemy you would like to add to your list?'
+            }
+        ]).then(answers => {
+            const sql = `INSERT INTO mylist (id) VALUES (?)`;
+            db.query(sql, answers.id, (err, response) => {
+                if(err) throw err;
+                console.log('Enemy added to your list!');
+                run();
+            })
+        })
+    })
+}
+
+const removeFromMyList = () => {
+    const sql = `SELECT * FROM mylist`;
+    db.query(sql, (err, response) => {
+        if(err) throw err;
+        console.table(response);
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the ID of the enemy you would like to remove from your list?'
+            }
+        ]).then(answers => {
+            const sql = `DELETE FROM mylist WHERE id = ?`;
+            db.query(sql, answers.id, (err, response) => {
+                if(err) throw err;
+                console.log('Enemy removed from your list!');
+                run();
+            })
+        })
+    })
+}
